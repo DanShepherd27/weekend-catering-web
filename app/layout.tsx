c3da6b6174a2b8090bd1475fd3ef1180ee307efc
Daniel Juhasz <jdani20001027@gmail.com>
2025-11-30 01:17:52 +0100

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import { Footer } from "@/components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Weekend Catering - Street Food Catering Services",
@@ -28,6 +29,7 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
